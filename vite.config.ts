27defe127ab359a9f4053f2e8c0c12b7a45024f3
lscheidh <lukasscheidhauer@gmx.de>
2026-01-03 18:56:51 +0100

    Ensure correct base path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/longier-planer/', // Wichtig: Ersetze mit deinem Repository-Namen
+      base: '/Longier-Planer/', // Wichtig: Ersetze mit deinem Repository-Namen
       server: {
         port: 3000,
         host: '0.0.0.0',
